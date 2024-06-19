87346508b7acb4174a20213e65fda7248f814ae7
King-BR <fernandopach2017@gmail.com>
2024-06-18 21:19:31 -0300

    Update exports

--- utils.js
+++ utils.js
@@ -12,6 +12,18 @@ function getCurrentDateTime() {
   return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
 
+function getCurrentDateTimeFile() {
+  const now = new Date();
+  const day = String(now.getDate()).padStart(2, "0");
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const year = String(now.getFullYear()).slice(-2);
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  return `${day}-${month}-${year}_${hours}-${minutes}-${seconds}`;
+}
+
 function getFormattedDate() {
   const now = new Date();
   const day = String(now.getDate()).padStart(2, "0");
@@ -51,6 +63,8 @@ const logger = {
 
 module.exports = {
   getCurrentDateTime,
+  getCurrentDateTimeFile,
+  getFormattedDate,
   logToFile,
   logger,
 };
