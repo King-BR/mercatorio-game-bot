31fe7e7e69a246ab51bda5bd036adbfc5b83b9bf
King-BR <fernandopach2017@gmail.com>
2024-09-23 18:57:22 -0300

    delete all

--- index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import main_routine from "./routines/main.js";
-
-// execute main routine once and then execute it every 1 hour
-setImmediate(main_routine);
-setInterval(main_routine, 1000 * 60 * 60);
