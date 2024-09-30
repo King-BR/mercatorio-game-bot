8f773a1710fba6f6d624c345179a9f38460adc34
King-BR <fernandopach2017@gmail.com>
2024-09-30 20:25:59 -0300

    Update pymerc

--- main.py
+++ main.py
@@ -29,12 +29,13 @@ async def main(
 
     while True:
         try:
-            current_turn = await turn(client)
+            current_turn = await turn(client, True)
             logger.info("Current turn: {}", current_turn)
 
             inventory = player.storehouse.items
 
             logger.info("Item | Purchase | Production | Imported | Sale | Consumed | Exported | Balance | Change")
+            logger.info("--------------------------------------------------------------------------------")
 
             # Get the player's inventory
             for item in inventory:
@@ -67,10 +68,8 @@ async def main(
                     balance = 0
 
                 change = purchase + production + imported - sale - comsumed - exported
-                #logger.info("{}: {} - Changed by {}", item.value, inventory[item].balance, change)
 
                 # print a inventory table
-                logger.info("--------------------------------------------------------------------------------")
                 logger.info("{} | {} | {} | {} | {} | {} | {} | {} | {}".format(item.value, purchase, production, imported, sale, comsumed, exported, balance, change))
                 logger.info("--------------------------------------------------------------------------------")
             
@@ -87,7 +86,7 @@ class TurnInProgressException(Exception):
 
     pass
 
-async def turn(client: Client) -> int:
+async def turn(client: Client, format: bool) -> int | str:
     """Get the current turn number.
 
     Args:
@@ -100,8 +99,35 @@ async def turn(client: Client) -> int:
 
     if "preparing next game-turn, try again in a few seconds" in response.text:
         raise TurnInProgressException("A turn is in progress")
+    
+    turnNumber = response.json()["turn"]
 
-    return response.json()["turn"]
+    if format:
+        # divide turnNumber by 12 to and round down to get the year
+        year = turnNumber // 12
+
+        # get the month by getting the remainder of turnNumber divided by 12
+        month = turnNumber % 12
+        
+        MONTH_STR = {
+            0: "January",
+            1: "February",
+            2: "March",
+            3: "April",
+            4: "May",
+            5: "June",
+            6: "July",
+            7: "August",
+            8: "September",
+            9: "October",
+            10: "November",
+            11: "December"
+        }
+
+        return f"{MONTH_STR[month]} {year}"
+    else:
+        return turnNumber
+
 
 
 if __name__ == "__main__":
